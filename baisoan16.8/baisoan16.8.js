06bd73bd68edaeeaeb8f5cd7aba57b245969361e
Dang Minh Nhat <dgminhnhat1991@gmail.com>
2024-08-16 02:12:47 +0800

    2 bài tập soạn trước

---
+++ baisoan16.8/baisoan16.8.js
@@ -0,0 +1,7 @@
+let diemThi = Number(prompt("Mời bạn nhập vào điểm thi cuối module"));
+if (diemThi >= 75){
+    alert("pass module");
+}
+else {
+    alert("fail");
+}
---
+++ baisoan16.8/baisoan16.8_2.js
@@ -0,0 +1,26 @@
+alert("Bạn chỉ được nhập số nguyên");
+let soNhap = Number(prompt("Mời bạn nhập vào ngày bất kỳ"));
+if (soNhap == 1) {
+    alert("ngày chủ nhật");
+}
+else if (soNhap == 2) {
+    alert("ngày thứ 2");
+}
+else if (soNhap == 3) {
+    alert("ngày thứ 3");
+}
+else if (soNhap == 4) {
+    alert("ngày thứ 4");
+}
+else if (soNhap == 5) {
+    alert("ngày thứ 5");
+}
+else if (soNhap == 6) {
+    alert("ngày thứ 6");
+}
+else if (soNhap == 7) {
+    alert("ngày thứ 7")
+}
+else {
+    alert("không phải ngày trong tuần");
+}
